49456155c4c5d45a7ffe8a2263cbc1a2f45b70e0
anuda_harish <anudadamsith@gmail.com>
2026-05-27 23:53:06 +0530

    create remove items from the list

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemList = document.getElementById('item-list');
 const itemInput = document.getElementById('item-input');
 const itemForm = document.getElementById('item-form');
+const clearBtn = document.getElementById('clear');
+const filter = document.getElementById('filter');
 
 function addItem(e){
   e.preventDefault();
@@ -17,6 +19,7 @@ function addItem(e){
   li.appendChild(button);
   itemList.appendChild(li);
   itemInput.value = '';
+  checkUI();
 
 }
 
@@ -34,4 +37,35 @@ function createIcon(classes){
   return icon;
 }
 
+function removeItem(e){
+  if(e.target.parentElement.classList.contains('remove-item')){
+    if(confirm("Are you sure?")){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+  checkUI();
+}
+
+function removeAll(){
+  while(itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI(){
+  const items = itemList.querySelectorAll('li');
+  if(items.length === 0){
+    clearBtn.style.display = 'none';
+    filter.style.display = 'none';
+  }else{
+    clearBtn.style.display = 'block';
+    filter.style.display = 'block';
+  }
+}
+
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', removeAll);
+checkUI();
